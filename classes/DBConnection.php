dcf35d9b827d2d0b9bc722cf10fda37eb88ceb3f
adiugale <adityaugale82@gmail.com>
2026-05-19 12:28:00 +0530

    updated railway mysql connection

--- classes/DBConnection.php
+++ classes/DBConnection.php
@@ -4,26 +4,34 @@ if(!defined('DB_SERVER')){
 }
 class DBConnection{
 
-    private $host = ('localhost:3307');
-    private $username = ('root');
-    private $password = ('');
-    private $database = ('vims_db');
+private $host;
+private $username;
+private $password;
+private $database;
    
     public $conn;
     
-    public function __construct(){
+  public function __construct(){
 
-        if (!isset($this->conn)) {
-            
-            $this->conn = new mysqli($this->host, $this->username, $this->password, $this->database);
-            
-            if (!$this->conn) {
-                echo 'Cannot connect to database server';
-                exit;
-            }            
-        }    
-        
+    $this->host = getenv('MYSQLHOST');
+    $this->username = getenv('MYSQLUSER');
+    $this->password = getenv('MYSQLPASSWORD');
+    $this->database = getenv('MYSQLDATABASE');
+
+    if (!isset($this->conn)) {
+
+        $this->conn = new mysqli(
+            $this->host,
+            $this->username,
+            $this->password,
+            $this->database
+        );
+
+        if ($this->conn->connect_error) {
+            die("Database Connection Failed: " . $this->conn->connect_error);
+        }
     }
+}
     public function __destruct(){
         $this->conn->close();
     }
